b37bcc93f4ab3a218e5c39d284f267814575ebdb
Rocco Colonna <colonna.rocco2000@gmail.com>
2021-03-22 18:41:31 +0100

    quasi finito

---
+++ dadi/js/script.js
@@ -0,0 +1,20 @@
+//chiedo se vuole giocare 
+prompt
+
+// numero casuale computer da 1 a 6
+var numeroCasualePc = Math.floor(Math.random() * 6) + 1;
+console.log(numeroCasualePc)
+
+//numero casuale utente da 1 a 6
+var numeroCasualeUtente = Math.floor(Math.random() * 6) + 1;
+console.log(numeroCasualeUtente)
+
+//comparo i due numeri e dico chi ha vinto
+var confrontoDadi;
+if (numeroCasualePc > numeroCasualeUtente) {
+    alert('Mi dispiace hai perso, riprova.')
+}else if (numeroCasualePc == numeroCasualeUtente){
+    alert('Hai pareggiato, riprova')
+} else{
+    alert('Bravo hai vinto.')
+}
